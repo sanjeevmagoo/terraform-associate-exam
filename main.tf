b14387b6d833136f393ab22278914c09e96e243e
Sanjeev Kumar <sanjeev.magoo@gmail.com>
2020-08-17 16:18:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.20.0"
+  version = "=0.12.29"
   features {}
 }
 
