641aa16038596e80823859887bb1cd0dac393949
Sanjeev Kumar <sanjeev.magoo@gmail.com>
2020-08-18 05:37:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = "0.12.20"
+}
+
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.23.0"
