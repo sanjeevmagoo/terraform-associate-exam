2ae02dffb8f75c7401ffce2ab5b01f9ecd7a5c62
Sanjeev Kumar <sanjeev.magoo@gmail.com>
2020-08-19 12:04:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,13 @@ terraform {
 
 terraform {   
  backend "azurerm" {     
- storage_account_name  = "csg1003200049146824"     
+ storage_account_name  = "csg1003200049146824 "     
  container_name        = "tstate"     
- key                   = "terraform.tfstate"   
- } 
+ key                   = "terraform.tfstate"
+ use_msi               = true
+ subscription_id       = "413dc411-db88-4292-b3a0-4bbf10eff75e"
+ tenant_id             = "872b50a3-f641-4a7d-b1c4-cce3c3da9318"   
+ }
 }
 
 provider "azurerm" {
