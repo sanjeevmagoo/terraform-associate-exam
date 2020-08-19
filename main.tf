232cc9b2039ff4756f49cb3a4865c9597d9c74dd
Sanjeev Kumar <sanjeev.magoo@gmail.com>
2020-08-19 12:07:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ terraform {
   required_version = ">= 0.12.20"
 }
 
-terraform {   
- backend "azurerm" {     
- storage_account_name  = "csg1003200049146824 "     
- container_name        = "tstate"     
- key                   = "terraform.tfstate"
- use_msi               = true
- subscription_id       = "413dc411-db88-4292-b3a0-4bbf10eff75e"
- tenant_id             = "872b50a3-f641-4a7d-b1c4-cce3c3da9318"   
- }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "cloud-shell-storage-centralindia"
+    storage_account_name = "csg1003200049146824"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
